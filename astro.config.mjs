bb7ca902f64391c895e3e208e9541522a801a5f5
Will Dravenstott <will.dravenstott@gmail.com>
2026-02-13 22:19:52 -0500

    Adding website to directory

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://wdrav.github.io',
+  // If your repo name is NOT <username>.github.io, uncomment and set this:
+  // base: '/your-repo-name',
+});
