8a44f0d8c2c5d6213357554da4c30920dfa6ea92
Will Dravenstott <will.dravenstott@gmail.com>
2026-02-13 22:26:44 -0500

    Fixing site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://wdrav.github.io',
   // If your repo name is NOT <username>.github.io, uncomment and set this:
-  // base: '/your-repo-name',
+  base: '/personal-site',
 });
